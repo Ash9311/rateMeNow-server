abf1f27759e94d514e4d6a4195fc79a413fb710e
@Ash9311 <ashutoshmulki7@gmail.com>
2024-01-03 15:47:26 +0530

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/config-server');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
