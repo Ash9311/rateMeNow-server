5219aef863c714080b63d25f3fe3f6d5a3f61ce9
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-01-26 23:47:28 +0530

    correction-config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const { PORT } = require('./config/config-server');
+const { PORT } = require('./config/config');
 
 app.use(cors());
 app.use(bodyParser.json());
